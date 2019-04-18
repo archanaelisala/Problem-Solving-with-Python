b7f7d0d73b885288ab81b1ca2322dc73954c1934
archanaelisala <48346714+archanaelisala@users.noreply.github.com>
2019-04-18 11:06:54 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,9 @@
+amount = float(input("Enter amount:"))
+Year = float(input("Enter Time Period in year:"))
+Rate = float(input("Enter the rate of interest:"))
+  
+# Calculates simple interest  
+SimpleInterest = (amount*Year*Rate) / 100
+  
+# Print the resultant value of SI  
+print("Simple Interest is", SimpleInterest) 
